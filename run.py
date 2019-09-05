c78c85e4dc3ac3e979005cb842d2d6d1fdb7302a
nishuranjan <nishantcont@gmail.com>
2019-09-06 00:38:11 +0530

    Added files for the prod release

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 from src.app import create_app
 
 if __name__ == '__main__':
-  env_name = "development"
+  env_name = "production"
   app = create_app(env_name)
   # run app
-  app.run()
+  app.run(host='0.0.0.0', port=9091)
