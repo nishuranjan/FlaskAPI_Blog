0542ab0d99b6151d3d55029bbe46ad77fd8ce1f4
nishuranjan <nishantcont@gmail.com>
2019-09-10 17:55:38 +0530

    Added the AWS PostgreSQL connection

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 from src.app import create_app
 
 if __name__ == '__main__':
-  env_name = "production"
+  env_name = "development"
   app = create_app(env_name)
   # run app
   app.run(host='0.0.0.0', port=9091)
--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = "hhgaghhgsdhdhdd"
-    SQLALCHEMY_DATABASE_URI = "postgresql://dnzociatsyinxn:09cd7945be0c0df341c33673920938bacdd9b87733c37b603b4dec02ec4df9b4@ec2-54-163-226-238.compute-1.amazonaws.com:5432/d585c2k4ch7dr0"
-
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:post2020APIDB@apidbfordev.clbolncrkftt.us-east-2.rds.amazonaws.com:5432/APIDBFORDEV"
+	
 class Production(object):
     """
     Production environment configurations
